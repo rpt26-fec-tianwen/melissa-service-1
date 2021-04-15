c6d94628c88aacedff99918f05a486c5377ea64a
Melissa Ryan <melissaryandigital@gmail.com>
2021-04-15 14:31:54 -0500

    Change to crossorigin and docker-compose and bundle build

--- server/server.js
+++ server/server.js
@@ -20,6 +20,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public/')));
 
+app.use('*', (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.get('/', (req, res) => {
   const path = __dirname.split('/');
